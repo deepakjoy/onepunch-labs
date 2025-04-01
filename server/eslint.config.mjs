5a364dc8b4409949b597879a99a57da63d63b294
Deepak Cheenath <deepakjc@gmail.com>
2025-04-01 18:49:47 +0530

    Initial commit with setup for a React + TypeScript + Vite application, including ESLint and Prettier configurations, basic server setup with Express, and essential project files.

---
+++ server/eslint.config.mjs
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,ts}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...prettierConfig.rules,
+    },
+  },
+];
